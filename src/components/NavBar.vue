d2d2840cc04c45eea3c5abaf71d6618c4159a631
paradox-3arthling <floydqaranja@gmail.com>
2020-02-26 11:14:22 +0300

    Changes so as to show the proper navigation bar depending on the device the connecting client is using

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,39 @@
 <template>
     <span>
-        <v-app-bar app dark>
-            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-            <v-toolbar-title>{{appTitle}} </v-toolbar-title>
+        <span class="hidden-md-and-up">
+            <v-app-bar app dark color="grey darken-3">
+                <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+                <v-spacer/>
+                <v-toolbar-title>{{appTitle}} </v-toolbar-title>
+            </v-app-bar> 
+            <v-navigation-drawer app v-model="drawer" disable-resize-watcher temporary>
+                <v-list nav>
+                    <template v-for="(item, index) in items">
+                        <v-list-item :key="index" v-bind:to="item.href" link>
+                            <v-list-item-content> {{ item.text }} </v-list-item-content>
+                        </v-list-item>
+                        <v-divider :key="index"/>
+                    </template>
+                </v-list>
+            </v-navigation-drawer>
+        </span> 
+        <!-- will add a crumb menu for medium up devices -->
+        
+        <v-app-bar app dark class="hidden-sm-and-down" color="grey darken-3">
+            <v-toolbar-title> {{appTitle}} </v-toolbar-title>
             <v-spacer/>
-            <v-btn text to="/">HOME</v-btn>
-            <v-btn text to="/about">ABOUT</v-btn>
-        </v-app-bar> 
+                <template v-for="(item, index) in items">
+                    <v-btn :key="index" v-bind:to="item.href" text> {{ item.text }} </v-btn>
+                </template>
+            <v-spacer/>
+        </v-app-bar>
+        <!-- <v-container fluid class="hidden-sm-and-down grey darken-3">
+            <v-row justify="center">
+                <v-col cols="auto">
+                    <v-breadcrumbs :items="items" divider="-" large></v-breadcrumbs>
+                </v-col>
+            </v-row>
+        </v-container> -->
     </span> 
 </template>
 
@@ -14,7 +41,11 @@
 export default {
     name: "NavBar",
     data: () => ({
-        drawer: false
+        drawer: false,
+        items: [
+          {text: 'HOME', href: '/'},
+          {text: 'ABOUT ME', href: '/about'}
+        ]
     }),
     props: {
         appTitle: String
