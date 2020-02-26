860d8b0e0da1e64b7ce19016663e7f8bdc539b9f
paradox-3arthling <floydqaranja@gmail.com>
2020-02-26 09:27:22 +0300

    Changes to ensure there is only one element in the root of the component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,23 @@
 <template>
-    
+    <span>
+        <v-app-bar app dark>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>{{appTitle}} </v-toolbar-title>
+            <v-spacer/>
+            <v-btn text to="/">HOME</v-btn>
+            <v-btn text to="/about">ABOUT</v-btn>
+        </v-app-bar> 
+    </span> 
 </template>
+
 <script>
 export default {
-    name: "NavBar"
+    name: "NavBar",
+    data: () => ({
+        drawer: false
+    }),
+    props: {
+        appTitle: String
+    }
 }
 </script>
