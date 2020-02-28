e2149cd23fa011100b0e4c9a10522b42ebb5ac92
paradox-3arthling <floydqaranja@gmail.com>
2020-02-28 16:35:02 +0300

    New component to be used as the site footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-footer height="auto" color="grey darken-3" dark>
+        <v-layout justify-center row wrap>
+            <p> &copy;2020. powered by Vue </p>
+        </v-layout>
+    </v-footer>
+</template>
