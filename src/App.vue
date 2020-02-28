eaf5760d394ba22fe1e935feccebfa18b02b4312
paradox-3arthling <floydqaranja@gmail.com>
2020-02-28 16:34:17 +0300

    Added a 'Footer' component to the site

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
     <v-content>
       <router-view></router-view>
     </v-content>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "App",
   components: {
-    NavBar
+    NavBar,
+    Footer
   }
 };
 </script>
