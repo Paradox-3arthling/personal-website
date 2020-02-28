316d27f19f1ec4aedbeb5c6990291d814539b333
paradox-3arthling <floydqaranja@gmail.com>
2020-02-28 10:25:21 +0300

    Putting all the element of `v-app-bar` in one element instead of 2 to display depending on device

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,18 @@
 <template>
     <span>
-        <span class="hidden-md-and-up">
             <v-app-bar app dark color="grey darken-3">
-                <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-                <v-spacer/>
+                <v-app-bar-nav-icon @click="drawer = true" class="hidden-md-and-up"></v-app-bar-nav-icon>
+                <v-spacer class="hidden-md-and-up"/>
+
                 <v-toolbar-title>{{appTitle}} </v-toolbar-title>
+
+                <v-spacer class="hidden-sm-and-down"/>
+                <template v-for="(item, index) in items" class="hidden-sm-and-down">
+                    <v-btn :key="index" v-bind:to="item.href" text> {{ item.text }} </v-btn>
+                </template>
+                <v-spacer class="hidden-sm-and-down"/>
             </v-app-bar> 
-            <v-navigation-drawer app v-model="drawer" disable-resize-watcher temporary>
+            <v-navigation-drawer app v-model="drawer" disable-resize-watcher temporary class="hidden-md-and-up">
                 <v-list nav>
                     <template v-for="(item, index) in items">
                         <v-list-item :key="index" v-bind:to="item.href" link>
@@ -16,24 +22,6 @@
                     </template>
                 </v-list>
             </v-navigation-drawer>
-        </span> 
-        <!-- will add a crumb menu for medium up devices -->
-        
-        <v-app-bar app dark class="hidden-sm-and-down" color="grey darken-3">
-            <v-toolbar-title> {{appTitle}} </v-toolbar-title>
-            <v-spacer/>
-                <template v-for="(item, index) in items">
-                    <v-btn :key="index" v-bind:to="item.href" text> {{ item.text }} </v-btn>
-                </template>
-            <v-spacer/>
-        </v-app-bar>
-        <!-- <v-container fluid class="hidden-sm-and-down grey darken-3">
-            <v-row justify="center">
-                <v-col cols="auto">
-                    <v-breadcrumbs :items="items" divider="-" large></v-breadcrumbs>
-                </v-col>
-            </v-row>
-        </v-container> -->
     </span> 
 </template>
 
