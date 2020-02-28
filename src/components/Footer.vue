5f66e1fa3f3af97aa0f5f23b4c6b2236a86d1d5b
paradox-3arthling <floydqaranja@gmail.com>
2020-02-28 17:47:22 +0300

    Made a workable footer still need some more styling(my opinion)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,40 @@
 <template>
     <v-footer height="auto" color="grey darken-3" dark>
-        <v-layout justify-center row wrap>
-            <p> &copy;2020. powered by Vue </p>
-        </v-layout>
+        <v-container>
+            <v-row>
+                <v-col v >
+                    <p> &copy;{{ current_year }}. Powered by Vue </p>
+                    <!-- <router-link to="/about"> Made by {{ user_s_name }} </router-link> -->
+                    <a href="https://github.com/Paradox-3arthling"> Made by {{ user_s_name }} </a>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-footer>
 </template>
+<script>
+export default {
+    name: "Footer",
+    data: () => ({
+        current_year: (new Date()).getFullYear()
+    }),
+    props: {
+        user_s_name: String
+    }
+}
+</script>
+<style scoped>
+    p, a {
+        text-decoration: none;
+        margin-left: 5%;
+        font-size: 12px;
+    }
+    .v-footer {
+        padding: 0px;
+    }
+    .container {
+        padding: 0.1%;
+    }
+    .v-application p {
+        margin-bottom: 0px;
+    }
+</style>
