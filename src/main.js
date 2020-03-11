7015b65da7188e37cacb3e330e750a9a875a19df
paradox-3arthling <floydqaranja@gmail.com>
2020-03-11 16:17:15 +0300

    initializing the 'VueGtag' plugin to be used in the project

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueGtag from "vue-gtag";
+
+Vue.use(VueGtag, {
+  config: {id: "UA-160193864-1"}
+}, router);
 
 Vue.config.productionTip = false;
 
